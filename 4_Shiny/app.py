0a7e01db532ca1996facafaba6a195f4a7aa9c13
Benjamin Gross <ben@benbox.org>
2024-06-08 21:44:00 +0200

    added map files

--- 4_Shiny/app.py
+++ 4_Shiny/app.py
@@ -1,15 +1,21 @@
 import pandas
+import io
 from shiny import App, render, ui, reactive
 from pathlib import Path
 from matplotlib import pyplot as plt
+from PIL import Image
+from pathlib import Path
+here = Path(__file__).parent
+nhs_areas = ["NHS Ayreshire and Arran", "NHS Borders", "NHS Dumfries and Galloway", "NHS Fife", "NHS Forth Valley", "NHS Grampian", "NHS Greater Glasgow and Clyde", "NHS Highland", "NHS Lanarkshire", "NHS Lothian", "NHS Orkney", "NHS Shetland", "NHS Tayside", "NHS Western Isles"]
 
 app_ui = ui.page_fluid(
     ui.layout_columns(
         ui.h1("Deaths in Scotland"),
-        ui.input_selectize("which_area", "Select Area(s)", choices=["NHS Ayrshire and Arran", "NHS Dumfries and Galloway", "NHS Forth Valley", "NHS Grampian", "NHS Grampian", "NHS Highland", "NHS Lothian", "NHS Orkney", "NHS Shetland", "NHS Western Isles", "NHS Fife", "NHS Tayside", "NHS Greater Glasgow and Clyde", "NHS Lanarkshire", ""], multiple=True, selected=["NHS Lothian"]),
+        ui.input_selectize("which_area", "Select Area(s)", choices=nhs_areas, multiple=True),
         ui.input_slider("which_year", "Select Year", min=2012, max=2021, value=2012, step=1, sep=""),
-        ui.output_plot("some_graph"),
-        ui.output_table("some_table"),
+        ui.output_plot("show_graph"),
+        ui.output_image("show_image"),
+        ui.output_table("show_table"),
     ),
 )
 
@@ -21,9 +27,35 @@ def server(input, output, session):
         df = pandas.read_csv(infile)
         return df
     
+    def create_map():
+        # Creating the map
+        #areas_list = input.which_area()
+        areas_list = ['NHS Ayreshire and Arran', 'NHS Borders', "NHS Dumfries and Galloway"]
+        image = 'Maps/Map.png'
+        file = here / image
+        img = Image.open(file)
+        map = Image.new("RGB", img.size, (255, 255, 255))
+        map.paste(img, box=(0, 0), mask=img.split()[3]) # 3 is the alpha channel
+        print('Areas: ', areas_list)
+        for area in areas_list:
+            image = 'Maps/' + nhs_areas[nhs_areas.index(area)].replace(" ", "_") + '.png'
+            file = here / image
+            img_overlay = Image.open(file)
+            img_overlay.load()  # required for png.split()
+            map.paste(img_overlay, box=(0, 0), mask=img.split()[3]) # 3 is the alpha channel
+        buffer = io.BytesIO()
+        #map.save(buffer, format="PNG")
+        map.save("Map_overlay.png")
+        print(len(buffer.getvalue()))
+        #img.save(buffer, format="PNG")
+        img.save("Map.png")
+        print(len(buffer.getvalue()))
+        #img = {"src": buffer.getvalue(), "width": "100px"}
+        #return img
+      
     @output
     @render.plot
-    def some_graph():
+    def show_graph():
         # Getting the data
         deaths = pandas.DataFrame( get_data())
         
@@ -54,10 +86,16 @@ def server(input, output, session):
         else:
           return None
         
+    # Visualizing the areas
+    @render.image
+    def show_image():
+        #img = {"src": here / "Map.png", "width": "100px"}
+        return create_map()
+        
     # Showing a table
     @output
     @render.table
-    def some_table():
+    def show_table():
         deaths = pandas.DataFrame( get_data() )
         
         # Choosing the data to be displayed
